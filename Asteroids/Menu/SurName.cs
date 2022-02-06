2f8fc3f6088012274b7e462cb63bd81641a3060c
Fkyeah <dimx63@mail.ru>
2022-02-06 14:11:22 +0300

    Меню игры

---
+++ Asteroids/Menu/SurName.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace Asteroids.Menu
+{
+    public class SurName : BaseText
+    {
+        public SurName(string value, Point position, Point direction, Size size) : base(value, position, direction, size)
+        {
+        }
+
+        public override void UpdatePosition()
+        {
+            if (Position.X > MainMenu.Width / 2 + 20)
+                Position.X += Direction.X;
+        }
+    }
+}
